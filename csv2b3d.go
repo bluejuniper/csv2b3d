8266535da0eabe8a1f4658f8670e1040b508bd4d
Art Barnes <abarnes@lanl.gov>
2022-04-18 07:02:28 -0600

    add struct

--- csv2b3d.go
+++ csv2b3d.go
@@ -4,11 +4,142 @@ import (
 	"fmt"
 	// "io"
 	"bufio"
+	"errors"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 )
+
+type FieldVector struct {
+	lat float64
+	lon float64
+	Ee  float64
+	En  float64
+}
+
+type CoordRange struct {
+	lat0    float64
+	lon0    float64
+	latStep float64
+	lonStep float64
+	nLat    int
+	nLon    int
+	nPoints int
+}
+
+func readLine(line string) (FieldVector, error) {
+	s := strings.Split(line, ",")
+
+	lat, err := strconv.ParseFloat(s[0], 64)
+
+	if err != nil {
+		return FieldVector{}, errors.New("Error parsing latitude")
+	}
+
+	lon, err := strconv.ParseFloat(s[1], 64)
+
+	if err != nil {
+		return FieldVector{}, errors.New("Error parsing longitude")
+	}
+
+	Ee, err := strconv.ParseFloat(s[2], 64)
+
+	if err != nil {
+		return FieldVector{}, errors.New("Error parsing Ee")
+		os.Exit(1)
+	}
+
+	En, err := strconv.ParseFloat(s[3], 64)
+
+	if err != nil {
+		return FieldVector{}, errors.New("Error parsing En")
+		os.Exit(1)
+	}
+
+	return FieldVector{lat: lat, lon: lon, Ee: Ee, En: En}, nil
+}
+
+func getRange(csvPath string) CoordRange {
+	fi, err := os.Open(csvPath)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to open %s for reading, aborting\n", csvPath)
+		os.Exit(1)
+	}
+
+	defer fi.Close()
+
+	scanner := bufio.NewScanner(fi)
+	scanner.Scan()
+
+	lats := map[float64]bool{}
+	lons := map[float64]bool{}
+	nPoints := 0
+
+	for scanner.Scan() {
+		nPoints++
+		vec, err := readLine(scanner.Text())
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading %s:%s: %v", csvPath, nPoints, err)
+			os.Exit(1)
+		}
+
+		lats[vec.lat] = true
+		lons[vec.lon] = true
+	}
+
+	lat0 := 0.0
+	lon0 := 0.0
+	latMax := 0.0
+	lonMax := 0.0
+
+	for k := range lats {
+		lat0 = k
+		latMax = k
+		break
+	}
+
+	for k := range lons {
+		lon0 = k
+		lonMax = k
+		break
+	}
+
+	for k := range lats {
+		if k < lat0 {
+			lat0 = k
+		}
+
+		if k > latMax {
+			latMax = k
+		}
+
+		break
+	}
+
+	for k := range lons {
+		if k < lon0 {
+			lon0 = k
+		}
+
+		if k > lonMax {
+			lonMax = k
+		}
+
+		break
+	}
+
+	fmt.Printf("latMax: %f, nLat: %d\nlonMax: %f, nLon: %d\n", latMax, len(lats), lonMax, len(lons))
+
+	return CoordRange{
+		lat0: lat0, lon0: lon0,
+		nLat: len(lats), nLon: len(lons),
+		latStep: (latMax - lat0) / float64(len(lats)), lonStep: (lonMax - lon0) / float64(len(lons)),
+		nPoints: nPoints,
+	}
+}
 
 func main() {
 	if len(os.Args) < 3 {
@@ -36,13 +167,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	cr := getRange(filepath.Join(csvFolder, csvFiles[0].Name()))
+	fmt.Fprintf(os.Stderr, "%f:%f:%d\n", cr.lat0, cr.latStep, cr.nLat)
+	fmt.Fprintf(os.Stderr, "%f:%f:%d\n", cr.lon0, cr.lonStep, cr.nLon)
+	fmt.Fprintf(os.Stderr, "%d\n", cr.nPoints)
+
 	for i, csvFile := range csvFiles {
 		if i >= 3 {
 			break
 		}
 
 		csvPath := filepath.Join(csvFolder, csvFile.Name())
-		fmt.Println(csvFile.Name())
+		fmt.Printf("%d: %s\n", i+1, csvFile.Name())
 
 		fi, err := os.Open(csvPath)
 
@@ -65,39 +201,16 @@ func main() {
 				break
 			}
 
-			fmt.Println(scanner.Text())
+			// fmt.Println(scanner.Text())
 			// Lat(Deg),Lon(Deg),Ee(V/km),En(V/km),Em(V/km)
-			s := strings.Split(scanner.Text(), ",")
-
-			lat, err := strconv.ParseFloat(s[0], 64)
+			vec, err := readLine(scanner.Text())
 
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing latitude on %s:%s, aborting\n", csvPath, j)
+				fmt.Fprintf(os.Stderr, "Error reading %s:%s: %v", csvFile, j, err)
 				os.Exit(1)
 			}
 
-			lon, err := strconv.ParseFloat(s[1], 64)
-
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing longitude on %s:%s, aborting\n", csvPath, j)
-				os.Exit(1)
-			}
-
-			Ee, err := strconv.ParseFloat(s[2], 64)
-
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing Ee on %s:%s, aborting\n", csvPath, j)
-				os.Exit(1)
-			}
-
-			En, err := strconv.ParseFloat(s[3], 64)
-
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing En on 1:%s, aborting\n", csvPath, j)
-				os.Exit(1)
-			}
-
-			fmt.Fprintf(fo, "%f,%f,%f,%f\n", lat, lon, Ee, En)
+			fmt.Fprintf(fo, "%f,%f,%f,%f\n", vec.lat, vec.lon, vec.Ee, vec.En)
 
 		}
 	}
