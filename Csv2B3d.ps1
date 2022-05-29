1db890c51f180efffda679500e911aa10c00b371
Art Barnes <abarnes@lanl.gov>
2022-05-29 08:11:19 -0600

    update powershell script

--- Csv2B3d.ps1
+++ Csv2B3d.ps1
@@ -1,7 +1,14 @@
 go build csv2b3d.go
-$BaseFolder = "$HOME\Data\GMD\norm_events"
-del data\*.b3d
 
-dir $BaseFolder | ForEach-Object -ThrottleLimit 8 -Parallel {    
+$InputFolder = "$HOME\Data\GMD\norm_events"
+$OutputFolder = "$HOME\Data\GMD\Events\B3D"
+
+if (!(Test-Path $OutputFolder)) {
+    New-Item -Path $OutputFolder -ItemType Directory
+}
+
+Remove-Item $OutputFolder\*.b3d
+
+Get-ChildItem $InputFolder | ForEach-Object -ThrottleLimit 8 -Parallel {    
     .\csv2b3d.exe $_.FullName "data\$($_.BaseName).b3d"
 }
